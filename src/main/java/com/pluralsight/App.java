9704a4ffb704def4c6189df2ad985c3982f72b97
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-07 11:04:44 -0400

    Added print statement "Learning to code is FUN!".

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("A nice little message");
         System.out.println("One more");
+        System.out.println("Learning to code is FUN!");
     }
 }
